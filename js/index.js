3ffdec5a7b6cde18acc5253524d25cff2769276e
FelixJentsch <FelixJentsch91@googlemail.com>
2023-11-22 12:05:58 +0100

    challenge solved

--- js/index.js
+++ js/index.js
@@ -10,3 +10,32 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+
+const postElement = document.createElement("article");
+postElement.classList.add("post");
+
+const postContentElement = document.createElement("p");
+postContentElement.classList.add("post__content");
+postContentElement.textContent =
+  "Lorem ipsum dolor, sit amet consectetur adipisicing elit.";
+
+const postFooterElement = document.createElement("footer");
+postFooterElement.classList.add("post__footer");
+
+const postUsernameElement = document.createElement("span");
+postUsernameElement.classList.add("post__username");
+postUsernameElement.textContent = "@username2";
+
+const postLikeButtonElement = document.createElement("button");
+postLikeButtonElement.classList.add("post__button");
+postLikeButtonElement.textContent = "♥ Like";
+/* postLikeButtonElement.setAttribute("data-js", "like-button"); */
+postLikeButtonElement.addEventListener("click", handleLikeButtonClick);
+
+postFooterElement.appendChild(postUsernameElement);
+postFooterElement.appendChild(postLikeButtonElement);
+
+postElement.appendChild(postContentElement);
+postElement.appendChild(postFooterElement);
+
+document.body.appendChild(postElement);
